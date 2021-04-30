2968c7d1d09c28b1187e1be76a865e81b1e2bb70
AlexAxenti <alex.axenti@gmail.com>
2021-04-30 02:49:46 -0400

    Set up posting and format for profile page

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const path = require("path");
 
 const app = express();
 app.use(express.static("public"));
+//app.use(express.static(__dirname + '/public/'));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,11 +22,22 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+/*                             Database Schemas                           */
+
 mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const postSchema = new mongoose.Schema({
+  username: String,
+  content: String,
+  posted: Date
+})
+const Post = new mongoose.model("Post", postSchema);
 
 const userSchema = new mongoose.Schema({
   username: String,
-  password: String
+  password: String,
+  bio: String,
+  posts: [postSchema]
 });
 userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model("User", userSchema);
@@ -33,17 +46,19 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+/*                             Get Requests                         */
+
 app.get("/", (req, res) => {
   res.render("index");
-})
+});
 
 app.get("/login", (req, res) => {
   res.render("login");
-})
+});
 
 app.get("/register", (req, res) => {
   res.render("register");
-})
+});
 
 app.get("/social", (req, res) => {
   if(req.isAuthenticated()){
@@ -52,7 +67,15 @@ app.get("/social", (req, res) => {
   } else {
     res.redirect("/login");
   }
-})
+});
+
+// app.get("/social", (req, res) => {
+//     res.render("social",{username:"testing"});
+// })
+
+app.get("/profile/testing", (req, res) => {
+  res.render("profile", {user:{username:"testing", posts:[{content:"hello there", posted:"2021-04-30"}, {content:"testing post", posted:"2021-04-25"}]}, currentUser:true});
+});
 
 app.get("/profile/:userName", (req, res) => {
   const userName = req.params.userName;
@@ -81,10 +104,42 @@ app.get("/profile/:userName", (req, res) => {
   }
 })
 
+/*                             Post Requests                         */
+
 app.post("/logout", (req, res) => {
   req.logout();
   res.redirect("/");
-})
+});
+
+app.post("/profile", (req, res) => {
+  //const username = req.user.username;
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  const yyyy = today.getFullYear();
+  const date = yyyy+"-"+mm+"-"+dd
+  console.log(date);
+  const post = new Post({
+    username: req.user.username,
+    content: req.body.postContent,
+    posted: date
+  });
+  User.findOne({username:req.user.username}, function(err, foundUser){
+    if(err){
+      console.log(err);
+    } else {
+      if(foundUser){
+        foundUser.posts.push(post);
+        foundUser.save();
+        res.redirect("/profile/"+req.user.username);
+      } else {
+        res.send("Error no user exists");
+      }
+    }
+  });
+});
+
+/*                            Login and Register                         */
 
 app.post("/login", (req, res) => {
   const user = new User({
@@ -109,7 +164,7 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  User.register({username: req.body.username}, req.body.password, function(err, user){
+  User.register({username: req.body.username, bio: ""}, req.body.password, function(err, user){
     if(err){
       console.log(err);
       res.redirect("/register");
